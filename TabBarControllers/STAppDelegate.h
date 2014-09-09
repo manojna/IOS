040f0f37bd3f835f13e31486252e3701d4169a8d
Paradigm <paradigm@Paradigms-Mac-mini.local>
2014-09-09 15:45:47 +0530

    Initial Commit

---
+++ TabBarControllers/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  TabBarControllers
+//
+//  Created by Paradigm on 9/9/14.
+//  Copyright (c) 2014 paradigmcreatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
