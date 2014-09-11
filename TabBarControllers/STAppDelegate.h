09a9223f77e8b5c5ddfc398dd53e849913590677
Paradigm <paradigm@Paradigms-Mac-mini.local>
2014-09-11 12:36:18 +0530

    TabBarView controllers

--- TabBarControllers/STAppDelegate.h
+++ TabBarControllers/STAppDelegate.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TabBarViewController.h"
 
 @interface STAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property(nonatomic,strong) UITabBarController *tabbar;
+@property(nonatomic,strong) UINavigationController *nav1,*nav2,*nav3,*nav4;
+
 
 @end
