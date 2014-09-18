8ca2b74ba9c8f782322001c951f39ef12674b35a
vamsikrishna24 <krishna.tangudu@paradigmcreatives.com>
2014-09-18 10:31:00 +0530

    Initial Commit

---
+++ Jsonassignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jsonassignment
+//
+//  Created by Paradigm on 9/18/14.
+//  Copyright (c) 2014 paradigmcreatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jsonassignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jsonassignment
+//
+//  Created by Paradigm on 9/18/14.
+//  Copyright (c) 2014 paradigmcreatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
